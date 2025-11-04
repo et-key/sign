14e0fb716200f9d4b052ed9215c4c5133e343c8a
Shin-ichi <johnny.shaman.saxophonist@gmail.com>
2025-11-04 10:58:25 +0000

    update a2 : lexer

--- proto/a2/lexer.js
+++ proto/a2/lexer.js
@@ -1,57 +1,30 @@
 
 'use strict';
 
-/*
-  tokenizPrefix:
-    - Add space after and tokenize prefix operators
-  tokenizePostfix:
-    - Add space before and tokenize postfix operators
-
-  preprocess:
-    - Remove control characters
-    - Remove comments
-    - Add spaces around binary operators
-    - Handle prefix operators
-    - Handle postfix operators
-  
-  tokenize:
-    - Split by line
-    - Handle indented blocks
-    - Handle brackets as blocks
-    - Split by spaces
-  
-  bracketToBlock:
-    - Convert bracketed tokens to nested arrays
-  
-  clean:
-    - Remove empty tokens
-*/
-
-
 const prepare = code => code
   .replace(/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x9F\xA0\xAD]/g, '')
   .replace(/^`[^\r\n]*(\r\n|[\r\n])/gm, '')
-  .replace(
-    /(\r\n|[\r\n])/g, '\r'                                  // Normalize line endings to \r
-  )
-  .replace(
-    /\\\r(?<!\\)\t+/g, '\\\n'                                       // Escaped \\\r to \\\n 
-  );
+  .replace(/(\r\n|[\r\n])/g, '\r')
+  .replace(/\\\r(?<!\\)\t+/g, '\\\n');
 
 const procUnary = code => code
   .replace(
-    /(\\[\s\S])|(`[^\r\n`]*`)|([#~!$@])([^\r\t#~!$@ ]+)([!~@])/g,
-    (_, $1, $2, $3, $4, $5) => ($1 || $2) || (
-      $3 && $5
-        ? `\x1F${$3}_\x1F${ procUnary($4) }\x1F_${$5}\x1F`
-        : $3 ? `\x1F${$3}_\x1F${ procUnary($4) }\x1F` : `\x1F${ procUnary($4) }\x1F_${$5}\x1F`
+    /(\\[\s\S])|(`[^\r\n`]*`)|([#~!$@])([^\r\t ]+)|([^\r\t ]+)([!~@])|([#~!$@])([^\r\t ]+)([!~@])/g,
+    (_, ...$) => ($[0] || $[1]) || (
+      $[2]
+        ? `\x1F${$[2]}_\x1F${ procUnary($[3]) }\x1F`
+        : $[5]
+          ? `${ procUnary($[4])}\x1F_${$[5]}\x1F`
+          : $[6] && $[8]
+            ? `\x1F_${$[6]}\x1F\x1F${ procUnary($[7]) }\x1F_${$[8]}\x1F`
+            : _
     )
   );
 
 const procBlock = code => code
   .replace(
-    /(\\[\s\S])|(`[^\r\n`]*`)|\r\t/gm,
-    (_, $1, $2) => ($1 || $2) || `\r\t\x1D`
+    /(\\[\s\S])|(`[^\r\n`]*`)|(?<!\\)\r(?<!\\)(\t+)/gm,
+    (_, $1, $2, $3) => ($1 || $2) || `\r${$3}\x1D`
   );
 
 const markSeparator = code => code
@@ -61,10 +34,14 @@ const markSeparator = code => code
   )
   .replace(
     /(`[^`\r\n]*`)|(?<!\\)(\[)|(?<!\\)(\])/g,
-    (_, $1, $2, $3) => $1 || ($2 && '\x1D[ ') || ($3 && ' ]\x1D')
+    (_, $1, $2, $3) => $1 || ($2 && '\x1D[\x1F') || ($3 && '\x1F]\x1D')
   )
   .replace(
-    /(\\[\s\S])|(`[^`\r\n]*`)|( +[:\#\,\?\~\+\-\*\/\^\|\;\&\<\=\>\!\%\'\@]+ +)/g,
+    /(\\[\s\S])|(`[^`\r\n]*`)|( *\, *)/g,
+    ( _, $1, $2, $3) => ($1 || $2) || `\x1F${$3}\x1F`
+  )
+  .replace(
+    /(\\[\s\S])|(`[^`\r\n]*`)|( +[\:\#\,\?\~\+\-\*\/\^\|\;\&\<\=\>\!\%\'\@]+ +)/g,
     (_, $1, $2, $3) => ($1 || $2) || `\x1F${$3}\x1F`
   );
 
@@ -80,4 +57,4 @@ module.exports = code => clean (
       unit => unit.split('\x1F')
     )
   )
-);
+).flat(Infinity);
