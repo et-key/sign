442b6f63146e3db1e74d9e239288af818b36f282
Shin-ichi <johnny.shaman.saxophonist@gmail.com>
2025-11-10 03:20:30 +0000

    implements a3 can be integral lexer and parser

---
+++ proto/a3/lexer.js
@@ -0,0 +1,71 @@
+
+'use strict';
+
+const prepare = code => code
+  .replace(/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x9F\xA0\xAD]/g, '')
+  .replace(
+    /((?<!\\)\\(?:\r\n|[\r\n]))|(`[^\r\n`]*`)|(\r\n|[\r\n])/g,
+    (_, $1, $2) => ($1 || $2) || '\r'
+  )
+  .replace(/^`[^\r]*/gm, '');
+
+const procBinary = code => code
+  .replace(
+    /(\\[\s\S])|(`[^\r\n`]*`)|([^\r\t ]+) +([\:\#\,\?\~\+\-\*\/\^\|\;\&\<\=\>\!\%\'\@]+) +([^\r\t ]+)( +([\:\#\,\?\~\+\-\*\/\^\|\;\&\<\=\>\!\%\'\@]+) +([^\r\t ]+))*/g,
+    (_, ...$) => ($[0] || $[1]) || (
+      `\x1F${$[2]}\x1F${ procBinary($[3]) }\x1F${ procBinary($[4]) }\x1F${ procBinary($[5]) }\x1F`
+    )
+  );
+
+const procUnary = code => code
+  .replace(
+    /(\\[\s\S])|(`[^\r\n`]*`)|([#~!$@])([^\r\t ]+)|([^\r\t ]+)([!~@])|([#~!$@])([^\r\t ]+)([!~@])/g,
+    (_, ...$) => ($[0] || $[1]) || (
+      $[2]
+        ? `\x1F${$[2]}_\x1F${ procUnary($[3]) }\x1F`
+        : $[5]
+          ? `${ procUnary( $[4] ) }\x1F_${ $[5] }\x1F`
+          : $[6] && $[8]
+            ? `\x1F_${$[6]}\x1F\x1F${ procUnary($[7]) }\x1F_${ $[8] }\x1F`
+            : _
+    )
+  );
+
+const procBlock = code => code
+  .replace(
+    /(\\[\s\S])|(`[^\r\n`]*`)|(?<!\\)\r(?<!\\)\t([^\r]+)/g,
+    (_, $1, $2, $3) => ($1 || $2) || `\r\x1D${ procBlock($3) }\x1D`
+  )
+  .replace(/(\\[\s\S])|(`[^\r`]*`)|\t/g, '$1$2');
+
+const markSeparator = code => code
+  .replace(
+    /(\\[\s\S])|(`[^`\r\n]*`)|(?<!\\)([\{\(])|(?<!\\)([\}\)])/g,
+    (_, $1, $2, $3, $4) => ($1 || $2) || ($3 && '[') || ($4 && ']')
+  )
+  .replace(
+    /(\\[\s\S])|(`[^`\r\n]*`)|(?<!\\)(\[)|(?<!\\)(\])/g,
+    (_, $1, $2, $3, $4) => ($1 || $2) || ($3 && '\x1D[\x1F') || ($4 && '\x1F]\x1D')
+  )
+  .replace(
+    /(\\[\s\S])|(`[^`\r\n]*`)|( *\, *)/g,
+    ( _, $1, $2, $3) => ($1 || $2) || `\x1E${$3}\x1E`
+  )
+  .replace(
+    /(\\[\s\S])|(`[^`\r\n]*`)| +([\:\#\,\?\~\+\-\*\/\^\|\;\&\<\=\>\!\%\'\@]+) +/g,
+    (_, $1, $2, $3) => ($1 || $2) || `\x1F${$3}\x1F`
+  );
+
+const clean = tokens => tokens
+  .map( t => Array.isArray(t) ? clean(t) : t )
+  .filter( t => t.length > 0 && t !== "[" && t !== "]");
+
+module.exports = code => clean (
+  markSeparator( procBlock( procUnary( prepare(code) ) ) ).split('\r')
+  .map(
+    line => line.split('\x1D')
+    .map(
+      unit => unit.split('\x1F')
+    )
+  )
+);
