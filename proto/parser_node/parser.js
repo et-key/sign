7e23969e9f0f4bdebf0340bb129d2e19cec8a6d6
Shin-ichi <johnny.shaman.saxophonist@gmail.com>
2025-11-26 08:18:58 +0000

    Thinking : SelfHost

--- proto/parser_node/parser.js
+++ proto/parser_node/parser.js
@@ -1,7 +1,8 @@
 const P = require("parsimmon");
 
 // 基本的なトークン定義
-const whitespace = P.regexp(/\s*/m);
+const string = P.regexp(/`[^`\r\n]*`/).desc("string");
+const comment = P.regexp(/^\t*`[^\r\n]*`?$/m).desc("comment");
+const char = P.regexp(/\\[\s\S]/).desc("char");
 const identifier = P.regexp(/[a-zA-Z_][a-zA-Z0-9_]*/).desc("identifier");
-const numberLiteral = P.regexp(/[0-9]+/).map(Number).desc("number");
-const stringLiteral = P.regexp(/"([^"\\]|\\.)*"/).desc("string");
+const number = P.regexp(/-?[0-9]+\.?[0-9]*/).desc("number");
