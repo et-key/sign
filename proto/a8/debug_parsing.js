6ec029de7471144bdbd74ee7453b30facbb0cbaf
et-key <etkey0x0@gmail.com>
2026-02-18 21:24:22 +0900

    Revert "add : a8(self host)"
    This reverts commit 5336775785e924eb0f1a89995693cae36deba644.

--- proto/a8/debug_parsing.js
+++
@@ -1,6 +0,0 @@
-import lexer from './prepare_lexer.js';
-import fs from 'fs';
-
-const code = fs.readFileSync('test_control.sn', 'utf8');
-const sexpr = lexer.parseToSExpr(code);
-console.log(JSON.stringify(sexpr, null, 2));
--- proto/a8/test_lexer_string.js
+++
@@ -1,7 +0,0 @@
-
-import helper from './prepare_lexer.js';
-const { parseToSExpr } = helper;
-
-const code = '1 # "Hello World"';
-const result = parseToSExpr(code);
-console.log(JSON.stringify(result, null, 2));
